7f65d92b191a11c4dc141422f0ccf2e7e721d13c
Dada80119 <93399757+Dada80119@users.noreply.github.com>
2023-11-04 12:59:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,73 @@
 const express = require('express');
-const cors = require('cors');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const cors = require('cors');
 
 const app = express();
-const port = 3001; // You can choose any available port
-
-// Configure the nodemailer transporter
-const transporter = nodemailer.createTransport({
-  service: 'Gmail', // e.g., 'Gmail', 'Yahoo', etc.
-  auth: {
-    user: 'dada80119@gmail.com',
-    pass: 'chkh lpoh jsxp muhe',
-  },
-});
-
 app.use(cors());
+const port = 3001;
+
 app.use(bodyParser.json());
 
-// Endpoint to send OTP to the provided email
-app.post('/send-otp', (req, res) => {
+// Generate a random OTP
+function generateOTP() {
+  return crypto.randomBytes(4).toString('hex');
+}
+
+// Store OTPs and email associations (use a database in a production environment)
+const otps = new Map();
+
+// Send email with OTP
+app.post('/sendOTP', (req, res) => {
   const { email } = req.body;
-  const otp = Math.floor(1000 + Math.random() * 9000);
+  const otp = generateOTP();
+
+  // Send the OTP via email (replace with your email sending logic)
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'dada80119@gmail.com',
+      pass: 'icaj dyqv joyx nqur',
+    },
+  });
+
   const mailOptions = {
-    from: 'your-email@example.com',
+    from: 'your-email@gmail.com',
     to: email,
-    subject: 'OTP Verification - Welcome to TrackMyPaisa',
-    text: `Your OTP is: ${otp}  to signup to our website. Never share OTP with anybody`,
+    subject: 'OTP Verification',
+    text: `Your OTP: ${otp}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.status(500).send('Error sending OTP');
+      res.status(500).send('Failed to send OTP');
     } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send(otp.toString());
+      // Store the OTP with the associated email (in-memory; use a database in production)
+      otps.set(email, { otp, timestamp: Date.now() });
+      res.status(200).send('OTP sent successfully');
     }
   });
 });
 
-// Add more endpoints for handling OTP verification, user registration, etc.
+// Verify OTP
+app.post('/verifyOTP', (req, res) => {
+  const { email, userOTP } = req.body;
+  const storedOTP = otps.get(email);
+
+  if (!storedOTP || Date.now() - storedOTP.timestamp > 300000) {
+    // OTP has expired (5 minutes)
+    res.status(401).send('OTP expired');
+  } else if (storedOTP.otp === userOTP) {
+    // OTP is correct
+    otps.delete(email); // Remove used OTP from storage
+    res.status(200).send('OTP verified successfully');
+  } else {
+    // Incorrect OTP
+    res.status(401).send('Invalid OTP');
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
